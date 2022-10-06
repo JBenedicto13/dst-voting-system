340cd5e37850dbc2768f740340a9af3f7e657862
jbenedicto20.jbb@gmail.com <JBenedicto13>
2022-10-06 18:52:28 +0800

    Add checking for email, wallet and pass

--- server/routes/check.js
+++ server/routes/check.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-router.get("/", async (req, res) => {
-    const email = req.body.email;
-    const profile = await User.findOne({email});
-    res.send(profile);
+router.get("/email", async (req, res) => {
+    const { email } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+        return res.status(400).send("User already exists with this email");
+    }
+})
+
+router.get("/wallet", async (req, res) => {
+    const { walletAddress } = req.body;
+    let user = await User.findOne({ walletAddress });
+    if (user) {
+        return res.status(400).send("User already exists with this wallet address");
+    }
+})
+
+router.get("/password", async (req, res) => {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+        const match = bcrypt.compareSync(password, user.password);
+        
+        if (match) {
+            return (
+                res.send(match)
+            )
+        }
+    }
 })
 
 module.exports = router;
