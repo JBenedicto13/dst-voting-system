0bb2077d217eaa23433c5fb34d4f3787a4890eeb
jbenedicto20.jbb@gmail.com <JBenedicto13>
2022-10-28 03:13:38 +0800

    Add smart contract but not yet done

--- client/src/components/election/Vote.js
+++ client/src/components/election/Vote.js
@@ -4,8 +4,36 @@ import candidate1 from '../../assets/candidate1.png';
 import candidate2 from '../../assets/candidate2.png';
 import dstsc_logo from '../../assets/logos/dstsc-logo.png';
 
+import { ethers } from 'ethers';
+// import ElectionSrc from '../../artifacts/contracts/Election.sol/Election.json';
+
+const ElectionContract = "0x500D68Ce0600AeE6ea671cB9Da0f9d1091f10a6E";
+
 const Vote = () => {
   const [selectedValue, setSelectedValue] = useState("Test");
+  const [chairperson, setChairperson] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const fetchChairperson = async () => {
+    if (typeof window.ethereum != "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(ElectionContract, process.env.ABI, signer);
+
+      setProvider(provider);
+      setSigner(signer);
+      setContract(contract);
+
+      try {
+        const data = await contract.chairperson();
+        console.log("data: ", data);
+      } catch (error) { 
+        console.log(error);
+      }
+    }
+  }
 
   function handleSubmit (e) {
     alert("Voted: " + selectedValue);
@@ -46,7 +74,7 @@ const Vote = () => {
                 </div>
                 <div className='btnRadio'>
                   <input className='btn-check' type="radio" name="gender" value="Male" id='govCandidate1' onChange={e => setSelectedValue(e.target.value)} autoComplete="off" />
-                  <label class="btn btn-outline-danger" for="govCandidate1">Select</label>
+                  <label className="btn btn-outline-danger" htmlFor="govCandidate1">Select</label>
                 </div>
               </div>
 
@@ -60,7 +88,43 @@ const Vote = () => {
                 </div>
                 <div className='btnRadio'>
                   <input className='btn-check' type="radio" name="gender" value="Female" id='govCandidate2' onChange={e => setSelectedValue(e.target.value)} autoComplete="off" />
-                  <label className="btn btn-outline-danger" for="govCandidate2">Select</label>
+                  <label className="btn btn-outline-danger" htmlFor="govCandidate2">Select</label>
+                </div>
+              </div>
+
+            </div>
+            
+          </div>
+          <div className='positionGroup mb-3'>
+            <div className='titleDiv'>
+              <h1>Vice Governor</h1>
+            </div>
+            <div className='cardsGroup'>
+              <div className='candidateCard' id='candidateCard1'>
+                <div className='card'>
+                    <div className='elipseBg'><img src={candidate1} alt='candidate'></img></div>
+                    <div className='candidateInfo'>
+                      <h2>Diana Almario</h2>
+                      <h3>Sample Partylist</h3>
+                    </div>
+                </div>
+                <div className='btnRadio'>
+                  <input className='btn-check' type="radio" name="gender" value="Male" id='govCandidate1' onChange={e => setSelectedValue(e.target.value)} autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="govCandidate1">Select</label>
+                </div>
+              </div>
+
+              <div className='candidateCard' id='candidateCard2'>
+                <div className='card'>
+                    <div className='elipseBg'><img src={candidate2} alt='candidate'></img></div>
+                    <div className='candidateInfo'>
+                      <h2>JB Benedicto</h2>
+                      <h3>Sample Partylist</h3>
+                    </div>
+                </div>
+                <div className='btnRadio'>
+                  <input className='btn-check' type="radio" name="gender" value="Female" id='govCandidate2' onChange={e => setSelectedValue(e.target.value)} autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="govCandidate2">Select</label>
                 </div>
               </div>
 
@@ -69,6 +133,10 @@ const Vote = () => {
           </div>
             <button className="btn btn-warning btnCastSubmit" type="submit" onSubmit={() => handleSubmit()}>Cast Vote</button>
         </form>
+        <div className='mb-5'>
+          <h1>{chairperson}</h1>
+          <button onClick={() => fetchChairperson()} className='btn btn-danger' type='button'>GetChairman</button>
+        </div>
     </div>
   )
 }
