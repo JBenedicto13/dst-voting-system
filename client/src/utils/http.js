126c078cd6425b02a332b5106bab6fbdc5bbcf30
jbenedicto20.jbb@gmail.com <JBenedicto13>
2023-06-07 20:10:35 +0800

    change base url

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: "http://localhost:3001/api"
-    // baseURL: "https://dst-voting-system.vercel.app/api"
-    baseURL: "https://voting-system-dst.vercel.app/api"
+    baseURL: "https://dst-voting-system.vercel.app/api"
+    // baseURL: "https://voting-system-dst.vercel.app/api"
 })
 
 export default instance;
