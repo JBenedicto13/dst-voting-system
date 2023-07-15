7b72c1a7e0e5206096cb9c46a00ca5115cf0bc98
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-07-16 01:54:50 +0800

    update baseURL

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001/api"
-    // baseURL: ''
+    // baseURL: "http://localhost:3001/api"
+    baseURL: "https://dst-voting-system-server.onrender.com/api"
 })
 
 export default instance;
